dbc0f7f20d981d52c6aa023ceb60242bd8d9dea8
Isaac1502 <isaactadinagiraldo02@gmail.com>
2024-01-30 21:20:22 -0500

    ship move to the right

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.get_rect()
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
@@ -15,6 +15,13 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+        # Movement flag; start with a ship that's not moving
+        self.moving_right = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         """Draw the ship at its current location."""
